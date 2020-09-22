ee3f4fa81ed29bf60490d52f0ff6537bd6f2b6b2
yafanisonya <yafanison@gmail.com>
2020-09-22 23:18:15 +0800

    jsx语法

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import logo from './logo192.png'
+import styles from "./indexModule.css"
 const name = "fanison";
 const obj = {
   firstName: 'Harry',
@@ -10,9 +11,26 @@ const obj = {
 function formatName (name) {
   return name.firstName + " " + name.lastName
 }
+const greet = <div>good</div>
+const show = false
+const a = [0, 1, 2, 3]
+
 const jsx = <div>
+  {/* //基本使用 表达式用{}包裹 */}
   <div>hello,{name}</div>
+  {/* 函数 */}
   <div>{formatName(obj)}</div>
+  {/* jsx对象 */}
+  {greet}
+  {/* 条件语句 */}
+  {show ? greet : '登录'}
+  {show && greet}
+  {/* 数组 */}
+  <ul>
+    {a.map(item => (<li key={item}>{item}</li>))}
+  </ul>
+  {/* 模块化、属性 */}
+  <img src={logo} className={styles.logo} style={{ width: "50px", height: "50px" }} ></img>
 </div>
 
 ReactDOM.render(jsx, document.getElementById("root"))
