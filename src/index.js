dbfa180cfe4788c6d9614d00451a7f5039eb763e
yafanisonya <yafanison@gmail.com>
2020-09-22 22:59:34 +0800

    函数表达式

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = "fanison";
+const obj = {
+  firstName: 'Harry',
+  lastName: 'Potter'
+}
+function formatName (name) {
+  return name.firstName + " " + name.lastName
+}
+const jsx = <div>
+  <div>hello,{name}</div>
+  <div>{formatName(obj)}</div>
+</div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(jsx, document.getElementById("root"))
+
