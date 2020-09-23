c1df841deaaea181ed80cc200071f78e36ad1db0
yafanisonya <yafanison@gmail.com>
2020-09-23 23:21:44 +0800

    function 组件

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ClassComponent from './pages/classComponent'
+import { FunctionComponent } from './pages/functionComponent';
 
 function App () {
   return (
     <div className="App">
       <ClassComponent></ClassComponent>
+      <FunctionComponent></FunctionComponent>
     </div>
   );
 }
--- src/pages/functionComponent.js
+++ src/pages/functionComponent.js
@@ -2,4 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 export function FunctionComponent (props) {
   const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
+  return (
+    <div><p>{date.toLocaleTimeString()}</p></div>
+  )
 }
